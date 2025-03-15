fa7057bee20ba8e75d3699aafdf3ab3a34869585
Alfonso Gonzalez <info@wsemantic.com>
2025-03-15 11:58:07 +0100

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -122,7 +122,7 @@ class ResPartner(models.Model):
                         'shopify_instance_id': shopify_instance_id.id,
                     })            
                     if skip_existing_customer:
-                        partner.write({'user_id',shopify_instance_id.salesperson_id.id if shopify_instance_id.salesperson_id else False})  # Asignar comercial                    
+                        partner.write({'user_id':shopify_instance_id.salesperson_id.id if shopify_instance_id.salesperson_id else False})  # Asignar comercial                    
                 if not skip_existing_customer:
                     vals_update = self.prepare_customer_vals(shopify_customer, shopify_instance_id)
                     partner.with_context(no_vat_validation=True).write(vals_update)
