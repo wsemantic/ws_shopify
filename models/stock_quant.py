e1ff6aa5bc26964749443860ada36ac8eb4d6761
Alfonso Gonzalez <info@wsemantic.com>
2025-05-05 13:46:48 +0200

    Update stock_quant.py

--- models/stock_quant.py
+++ models/stock_quant.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 import logging
+import datetime
 from odoo import api, models, fields
 
 logger = logging.getLogger(__name__)
